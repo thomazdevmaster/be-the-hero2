b50c9f2d296a409affb1a66168916be43549947f
Vinicius Pimenta <62510504+vinicius-pimenta@users.noreply.github.com>
2021-09-29 10:42:18 -0300

    Update routes.js

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -11,15 +11,15 @@ const routes = express.Router();
 routes.post('/sessions', SessionController.create);
 
 routes.get('/ongs', OngController.index);
-routes.post('/ongs', celebrate({
-  [Segments.BODY]: Joi.object().keys({
-    name: Joi.string().required(),
-    email: Joi.string().required().email(),
-    whatsapp: Joi.string().required().min(10).max(11),
-    city: Joi.string().required(),
-    uf: Joi.string().required().length(2),
-  })
-}), OngController.create);
+// routes.post('/ongs', celebrate({
+//   [Segments.BODY]: Joi.object().keys({
+//     name: Joi.string().required(),
+//     email: Joi.string().required().email(),
+//     whatsapp: Joi.string().required().min(10).max(11),
+//     city: Joi.string().required(),
+//     uf: Joi.string().required().length(2),
+//   })
+// }), OngController.create);
 
 routes.get('/profile', celebrate({
   [Segments.HEADERS]: Joi.object({
